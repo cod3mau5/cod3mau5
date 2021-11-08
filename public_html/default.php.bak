3b4a0f8d7787304493302728ead4de90195e1d70
cod3mau5 <team@codemau5.com>
2021-11-08 04:11:12 +0000

    initial commit

---
+++ public_html/default.php.bak
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Welcome to codemau5!</title>
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="Default page" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet"> -->
+    <style>
+        body, html{
+            height: 100%;
+            margin: 0;
+            font-family: "Lobster Two";
+        }
+        /* the hero image */
+        .hero-image{
+            background-color: #000000;
+            /* hero image used as background*/
+            background-image: url('/img/LogoCompleto.png');
+            /* set a specific height for hald hero use 50% for full use 100% */
+            height: 100%;
+            /* positioning and centering the image to scale nicely on all screens */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+            position: relative;
+        }
+
+        /* styling and centering the hero text */
+        .hero-text{
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            background: rgba(0, 0, 0, 0.77);
+            padding: 0 40px;
+            height:100%;
+        }
+        h1{
+            font-size: 4em;
+            color: #FFFDE7;
+            text-shadow: 3px 4px 1.1px #4C3643;
+        }
+        P{
+            font-size: 2em;
+            color: #F57F17;
+        }
+        /* styling the hero button */
+        .hero-text button{
+            font-family: "Open Sans";
+            font-size: 1.1em;
+            border: 2px solid rgba(254, 209, 54, 0.7);
+            background-color: rgba(254, 209, 54, 0.2);
+            display: inline-block;
+            padding: 10px 20px;
+            color: #fff;
+            text-align: center;
+            cursor:pointer;
+            transition: .6s;
+            border-radius: 0 10px 0 10px;
+        }
+
+        .hero-text button:hover{
+            transition: 0.6s;
+            background-color: rgba(254, 209, 54, 0.7);
+        }
+    </style>
+</head>
+
+<body>
+<div class="hero-image">
+	<div class="hero-text">
+		<h1>welcome to codemau5 web site</h1>
+		<p>Here you can find a professional web-developer</p>
+		<a href="repository.php">
+            <button>Click here to see what codemau5 is up to</button>
+        </a>
+	</div>
+</div>
+
+</body>
+
+</html>
---
+++ public_html/repository.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Works</title>
+
+    <style>
+        html {
+            font-family: "Poppins", sans-serif;
+            font-size: 62.5%;
+            word-spacing: 0.1rem;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            box-sizing: border-box;
+        }
+
+        *,
+        *::before,
+        *::after {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            color: #26282d;
+            font-weight: 100;
+            line-height: 2.5rem;
+            background:#12181b;
+        }
+
+        .cards {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .grid__container {
+            display: flex;
+            flex-direction: column;
+            padding: 1rem 1.8rem 4rem 1.6rem;
+            background: #12181b;
+            background-image: url('/img/LogoCompleto.png');
+            background-position: center;
+            background-repeat: repeat;
+            background-size: 50px;
+            position: relative;
+            height:100%;
+        }
+
+        .grid__title {
+            margin: 3rem 0 2rem 0;
+            font-size: 3rem;
+            color: white;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        .grid__title span{
+            padding: 1.5rem 2.5rem;
+            background-color: rgba(254, 209, 54, 0.77);
+            text-shadow: 2px 2px 5px rgba(21, 255, 36, 1);
+            border-radius: 10px;
+            width: 40%;
+            margin: 0 auto;
+        }
+        .grid__item {
+            margin: 2rem 0;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.38);
+            background: #2a2e35;
+            border-radius: 0.6rem;
+            padding: 2.5rem;
+        }
+        .grid__item:hover {
+            box-shadow: 0 10px 20px rgba(3, 27, 78, 0.1);
+        }
+        .grid__item-category {
+            color: #99a1b3;
+            text-transform: uppercase;
+            font-weight: 300;
+            letter-spacing: 0.2rem;
+            font-size: 1.6rem;
+        }
+        .grid__item-title {
+            margin-top: 2rem;
+            font-size: 2rem;
+            font-weight: 600;
+            color: #fff;
+        }
+        .grid__item-excerpt {
+            margin-top: 1.5rem;
+            font-size: 1.8rem;
+            color: #b2becd;
+        }
+
+        @media (min-width: 768px) {
+            .cards {
+                height: 100vh;
+                align-items: center;
+            }
+
+            .grid__container {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                grid-template-rows: repeat(2, minmax(10rem, auto));
+                padding: 1rem 4rem 4rem 4rem;
+                column-gap: 2rem;
+            }
+
+            .grid__title {
+                margin-top: 3rem;
+                grid-column: 1 / 3;
+                font-size: 3rem;
+            }
+        }
+        @media (min-width: 1200px) {
+            .card {
+                height: 100%;
+            }
+
+            .grid__container {
+                grid-template-columns: repeat(4, 1fr);
+                grid-template-rows: repeat(1, minmax(auto));
+            }
+
+            .grid__title {
+                margin-top: 3rem;
+                grid-column: 1 / 5;
+                font-size: 3rem;
+            }
+        }
+        .w-100{
+            max-width:100%;
+        }
+    </style>
+</head>
+<body>
+    <section class="cards">
+    <div class="grid__container">
+        <h1 class="grid__title"><span>SEE OUR WORKS</span></h1>
+        <!-- Grid item START -->
+        <div class="grid__item">
+            <h2 class="grid__item-title">Mahi Fishing Charters</h2>
+            <p class="grid__item-excerpt">
+                <img src="/img/Screenshot_MahiFishingCharters.jpg" class="w-100">
+            </p>
+            <h3 class="grid__item-category">e-commerce for fishing reservation</h3>
+        </div>
+        <!-- Grid item END -->
+
+        <!-- Grid item START -->
+        <div class="grid__item">
+        <h2 class="grid__item-title">Coming soon...</h2>
+
+        </div>
+        <!-- Grid item END -->
+
+        <!-- Grid item START -->
+        <div class="grid__item">
+        <h2 class="grid__item-title">Coming soon...</h2>
+        <p class="grid__item-excerpt">
+         
+        </p>
+        </div>
+        <!-- Grid item END -->
+
+        <!-- Grid item START -->
+        <div class="grid__item">
+        <h2 class="grid__item-title">Coming soon...</h2>
+
+        </div>
+        <!-- Grid item END -->
+    </div>
+    </section>
+</body>
+</html>
